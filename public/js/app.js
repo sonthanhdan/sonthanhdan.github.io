9d00a3b79058505c3dbdce40f7539dab922a7bc3
sonthanhdan <thanhdan1995@gmail.com>
2017-07-09 14:13:17 +0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,1 @@
-var Logo = React.createClass({
-    render: function(){
-        return(
-            <h2>React</h2>
-        );
-    }
-});
 
-
-ReactDOM.render(
-    
-    <Logo />
-    
-
-, document.getElementById('react-logo')
-);
